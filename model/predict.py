82fb5d343ef152796471e99cf437823c78d163fa
Muhammed Mert Bek <148920150+mertbek10@users.noreply.github.com>
2025-08-16 18:18:53 +0300

    testler yapıldı

---
+++ model/predict.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from xgboost import XGBClassifier
+
+#modeli yüklüyoruz 
+model = XGBClassifier()
+model.load_model("saved_models/xgb_onehot.json")
+
+#feature isimlerini oku
+with open("saved_models/xgb_onehot_features.txt", "r") as f:
+    feature_names = f.read().strip().split(",")
+
+print(f"Model yüklendi ({len(feature_names)} feature var)")
+
+#tahminler için input alma
+input_values =[]
+for feature in feature_names:
+    val = input(f"{feature} değerini gir : ")
+    try :
+        #sayısal featureleri floata çevir
+        val = float(val)
+
+    except ValueError:
+        #buraya dönüş yapılcak 
+        pass
+    input_values.append(val)
+
+
+# 4. DataFrame oluştur
+user_input = pd.DataFrame([input_values], columns=feature_names)
+mapping = {0: "Adelie", 1: "Chinstrap", 2: "Gentoo"}
+
+# 5. Tahmin yap
+prediction = model.predict(user_input)[0]
+print("Tahmin : ", mapping.get(prediction, "Bilinmeyen sınıf"))
+
